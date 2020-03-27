8251d65ef74ed7c6cf8d81d720c8f230ca7df158
Leeroy Phili <leeroyphili@gmail.com>
2020-03-27 17:50:58 -0400

    adding the uv index function to the script

--- script.js
+++ script.js
@@ -64,18 +64,91 @@ function makeList() {
 
     // add to page
     city.append(cityDate, image)
-    cardBody.append(city,temperature, humidity, wind);
+    cardBody.append(city,temperature, humidity, wind,);
     card.append(cardBody);
     $("#currentCity").append(card)
+
+    var lon = response.coord.lon;
+    var lat = response.coord.lat;
+
+    // SEND OVER TO uvIndex()
+    uvIndex(lon, lat);
    
   }
 
+// making a request to get the UV index base on longitute and the latittude of the input value 
+ 
+function uvIndex(lon, lat) {
+	// SEARCHES
+	var indexURL =
+		"https://api.openweathermap.org/data/2.5/uvi?appid=0992b5cf68ff327e9b1a50f58b53ba8b&lat=";
+	var middle = "&lon=";
+	var indexSearch = indexURL + lat + middle + lon;
+	console.log(indexSearch);
 
-//   function getForecast(){
+	$.ajax({
+		url: indexSearch,
+		method: "GET"
+	}).then(function(response) {
+		var uvFinal = response.value;
 
-// console.log('...')
+		// should be able to compare float to the numbers, try it out
+		// then append button with uvFinal printed to it
+		$("#currentCity").append("UV Index: ");
+		var uvBtn = $("<button>").text(uvFinal);
+        $("currentCity").append(uvFinal);
+    })
+}
 
-//   };
+
+
+        // function getCurrentForecast () {
+  
+        //     $.ajax({
+        //       url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey,
+        //       method: "GET"
+        //     }).then(function (response){
+          
+        //       console.log(response)
+        //       console.log(response.dt)
+        //       $('#forecast').empty();
+          
+            //   // variable to hold response.list
+            //   let results = response.list;
+            //   console.log(results)
+              
+            //   for (let i = 0; i < results.length; i++) {
+          
+            //     let day = Number(results[i].dt_txt.split('-')[2].split(' ')[0]);
+            //     let hour = results[i].dt_txt.split('-')[2].split(' ')[1];
+            //     console.log(day);
+            //     console.log(hour);
+          
+            //     if(results[i].dt_txt.indexOf("12:00:00") !== -1){
+                  
+            //       // get the temperature and convert to fahrenheit 
+            //       let temp = (results[i].main.temp - 273.15) * 1.80 + 32;
+            //       let tempF = Math.floor(temp);
+          
+            //       const card = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+            //       const cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+            //       const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+            //       const temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+            //       const humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + results[i].main.humidity + "%");
+          
+            //       const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + results[i].weather[0].icon + ".png")
+          
+            //       cardBody.append(cityDate, image, temperature, humidity);
+            //       card.append(cardBody);
+            //       $("#forecast").append(card);
+          
+            //     }
+            //   }
+            // });
+          
+        //   })}
+          
+
 
 
 
